3f0c9135922dcdfbd39954cced8bfabe8841bcda
FizzFizz <fizz.from.outerspac@gmail.com>
2021-02-09 13:01:17 +0900

    master

---
+++ SubWindow.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+
+namespace GD_URIConv
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SubWindow : Window{
+        public SubWindow(){
+            InitializeComponent();
+            
+        }
+
+    }
+
+}
