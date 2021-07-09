92e624b622a4f09d7e155cb50b5bcefc8e4c15d5
FizzFizz <fizz.from.outerspac@gmail.com>
2021-07-09 23:30:57 +0900

    test1.2

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Interop;
+
+namespace GD_URIConvert
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window{
+
+        //メンバ変数
+        private const string HELP_DESCRIPTION
+            = "説明\n\n"
+            + "このアプリケーションはGoogleDriveにアップロードされた画像の共有URLを直接リンクに変換します。\n\n"
+            + "例\n\n"
+            + "変換前\n"
+            + "https://drive.google.com/file/d/{ContentsID}\n"
+            + "or\n"
+            + "https://drive.google.com/open?id={ContentsID}\n\n"
+            + "変換後\n"
+            + "https://drive.google.com/uc?export=view&id={ContentsID}";
+            
+        private const string TEXTBOX_DEFAULT_TEXT = "URLを入力してください。";
+
+        private ClipboardHelper IClipboardHelper;
+    
+        public ObservableCollection<ListItem> ListItems;
+
+        //コンストラクタ
+        public MainWindow(){
+            InitializeComponent();
+            ListItems = new ObservableCollection<ListItem>();
+            this.DataContext = ListItems;
+            this.Loaded += MainWindow_Loaded;
+            this.Closing += MainWindow_Closing;
+
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e){
+            IClipboardHelper.Stop();
+        }
+        
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e){
+            IClipboardHelper = new ClipboardHelper(new WindowInteropHelper(this).Handle);
+            IClipboardHelper.DrawClipboard += ClipboardHelper_DrawClipboard;
+            IClipboardHelper.Start();
+        }
+
+        //イベントハンドラここから
+        private void Button_Submit_Click(object sender, RoutedEventArgs e){
+            AddList(URL_Text.Text);
+
+        }
+
+        private void Button_Copy_Click(object sender, RoutedEventArgs e){
+            SetClipboard(URLList.SelectedIndex);
+
+        }
+
+        private void Button_Erase_Click(object sender, RoutedEventArgs e){
+            int itemIndex = URLList.SelectedIndex;
+            
+            if(itemIndex >= 0){ListItems.RemoveAt(index: itemIndex);}
+
+        }
+
+        private void Button_Clear_Click(object sender, RoutedEventArgs e){
+            ListItems.Clear();
+
+        }
+        
+        private void Button_Help_Click(object sender, RoutedEventArgs e){
+            MessageBox.Show(messageBoxText: HELP_DESCRIPTION, caption: "Help");
+
+        }
+
+        private void Button_SubWindowOpen_Click(object sender, RoutedEventArgs e){
+            SubViewOpen(URLList.SelectedIndex);
+
+        }
+
+        private void OnGotFocusHandler(object sender, RoutedEventArgs e){
+            if(URL_Text.Text == TEXTBOX_DEFAULT_TEXT){URL_Text.Text = "";}
+
+        }
+
+        private void OnLostFocusHandler(object sender, RoutedEventArgs e){
+            if(URL_Text.Text == ""){
+                URL_Text.Text = TEXTBOX_DEFAULT_TEXT;
+                URL_Text.Background = Brushes.Transparent;
+                
+            }
+
+        }
+
+        private void ClipboardHelper_DrawClipboard(object sender, EventArgs e){
+            AddList(Clipboard.GetText());
+        }
+        //イベントハンドラここまで
+        
+    }
+
+}
